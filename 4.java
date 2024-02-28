a70ea10dc2ade3e4fac6f0e7d3d8a223cfede445
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-02-28 22:56:05 +0700

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,21 @@
+public class ParentClass {
+    ParentClass(){
+        System.out.println("Constructor of Parent");
+    }
+    void disp(){
+        System.out.println("Parent Method");
+    }
+}
+class JavaExample extends ParentClass{
+    JavaExample(){
+        System.out.println("Constructor of Child");
+    }
+    void disp(){
+        System.out.println("Child Method");
+        super.disp();
+    }
+    public static void main(String[] args) {
+        JavaExample obj = new JavaExample();
+        obj.disp();
+    }
+}
