7fdcb61e416d6e19dfc36593bf1583fc6ae6683c
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-02-28 22:49:49 +0700

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,17 @@
+package work;
+public class Animal {
+void eat(){System.out.println("eating....");}  
+}
+
+class Dog extends Animal{
+    void bark(){System.out.println("barking....");}
+}
+class Testinheritance{
+    public static void main(String[] args) {
+    Dog d = new Dog();
+        d.eat();
+        d.bark();
+        
+    }
+   
+}
