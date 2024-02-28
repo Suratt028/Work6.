d4fec612db4739601b88fc07a3a9c771b871b626
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-02-28 23:01:15 +0700

    Create 7.3.java

---
+++ 7.3.java
@@ -0,0 +1,14 @@
+package Ex7;
+
+public class Dog extends Animal{
+    public Dog(){
+        super();
+        System.out.println("A new Dog has been created!");
+    }
+    public void sleep(){
+        System.out.println("A Dog sleeps...");
+    }
+    public void eat(){
+        System.out.println("A Dog eats");
+    }
+}
